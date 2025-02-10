3f365ca455fa24724d3271164a03b20707411c18
ziamnoun <ziamziam05@gmail.com>
2025-02-10 17:07:49 +0600

    i am back

---
+++ src/Services/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const loading = () => {
+    return (
+        <div>
+            <span className="loading loading-spinner loading-lg"></span>
+        </div>
+    );
+};
+
+export default loading;
